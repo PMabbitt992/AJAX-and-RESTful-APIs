d5cba3d67e97c1c3f0d8326594d4b0f4f3d2890f
Paige <pmabbitt992@west-mec.org>
2019-09-27 13:49:47 -0700

    Comments

--- Projects/Project_Lab/www/StockScript.js
+++ Projects/Project_Lab/www/StockScript.js
@@ -12,7 +12,7 @@ var httpRequest = false;
 var entry = "MSFT";
 
 
-
+// Instantiate a new request in variable httpRequest
 function getRequestObject() {
 
     try {
@@ -22,7 +22,7 @@ function getRequestObject() {
     }
     return httpRequest;
 }
-
+//prevent the default action from happening
 function stopSubmission(evt) {
 
     if (evt.preventDefault) {
@@ -35,23 +35,28 @@ function stopSubmission(evt) {
 
 
 function getQuote() {
+    //if there is input, make entry equal to the entered string
     if (document.getElementsByTagName("input")[0].value) {
         entry = document.getElementsByTagName("input")[0].value;
+        //if there is no httpRequest, make one
         if (!httpRequest) {
             httpRequest = getRequestObject();
         }
+        //set up conditions for getting data three, two, and one days ago, and current day
         let current = new Date();
-
         let threeDaysAgo = new Date();
         threeDaysAgo.setDate(current.getDate() - 3);
         let dateOne = `2017-${getMonth(threeDaysAgo.getMonth())}-${threeDaysAgo.getDate()}`;
         let dateTwo = `2017-${getMonth(current.getMonth())}-${current.getDate()}`
+        //abort any outstanding requests, open a new one to the url, and send it
         httpRequest.abort();
         httpRequest.open("get", `StockCheck.php?t=${entry}&s=${dateOne}&e=${dateTwo}`, true);
         httpRequest.send(null);
+        //when the ready state change is changed, run displayData()
         httpRequest.onreadystatechange = displayData;
     }
 
+    //get the month for data requests
     function getMonth(month) {
         var curMonth = month > 0 ? month + 1 : 12;
         return curMonth < 10 ? "0" + curMonth : curMonth;
@@ -59,10 +64,13 @@ function getQuote() {
 
     function displayData() {
         if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            //put the data response into a variable and open the stockItems variable
             var stockResults = httpRequest.responseText;
             var stockItems;
             try {
+                //parse stock results and store in stock items
                 stockItems = JSON.parse(stockResults);
+                //input data into the table in correct slots
                 document.getElementById("ticker").innerHTML = stockItems.dataset.dataset_code;
                 document.getElementById("openingPrice").innerHTML = stockItems.dataset.data[0][1];
                 document.getElementById("lastTrade").innerHTML = stockItems.dataset.data[1][4];
@@ -72,7 +80,7 @@ function getQuote() {
                 document.getElementById("volume").innerHTML = stockItems.dataset.data[0][5];
                 return;
             } catch (error) {
-                console.log('test');
+                //Error message is the code doesn't work
                 document.getElementById("ticker").innerHTML = "Error: Invalid Ticker.";
                 document.getElementById("openingPrice").innerHTML = "-";
                 document.getElementById("lastTrade").innerHTML = "-";
@@ -87,6 +95,7 @@ function getQuote() {
     }
 }
 
+//create the table
 function formatTable() {
     var rows = document.getElementsByTagName("tr");
     for (var i = 0; i < rows.length; i++) {
@@ -95,6 +104,7 @@ function formatTable() {
 }
 
 
+//event listeners for eah function that needs one
 var form = document.getElementsByTagName("form")[0];
 if (form.addEventListener) {
     form.addEventListener("submit", stopSubmission, false);
